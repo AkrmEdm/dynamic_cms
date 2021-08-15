6236977a49e00eb310e1bad8343b42be5a7aea64
akrmedm-dev <akrmedm@gmail.com>
2021-08-15 10:12:16 +0500

    fix js

--- partials/footer.php
+++ partials/footer.php
@@ -4,11 +4,13 @@
 
   <!-- <script src="<?= $domain ?>/js/bootstrap.bundle.min.js"
     integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"></script> 
 
   <script src="../js/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
-    crossorigin="<?= $domain ?>/js/bootstrap.bundle.min.js"></script>
+    crossorigin="<?= $domain ?>/js/bootstrap.bundle.min.js"></script> -->
 
+  <script src="<?= $domain ?>/js/bootstrap.bundle.min.js"></script>
+  
   <script src="<?= $domain ?>/js/feather.min.js"></script>
   
   <script src="<?= $domain ?>/js/dashboard.js"></script>
